0ef792e761bb0c6338616d61efa3d16be9b6dd5a
anderson_vieira <99267104+AndersonVieir4@users.noreply.github.com>
2023-09-13 10:05:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
 
-export default function App() {
+export default function CadastroScreen() {
+  const [nome, setNome] = useState('');
+  const [genero, setGenero] = useState('');
+  const [dataNascimento, setDataNascimento] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmarEmail, setConfirmarEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [idioma, setIdioma] = useState('');
+  const [dadosCadastrados, setDadosCadastrados] = useState(null);
+
+  const handleCadastro = () => {
+    const dados = {
+      Nome: nome,
+      Gênero: genero,
+      'Data de Nascimento': dataNascimento,
+      Usuário: usuario,
+      Senha: senha,
+      'E-mail': email,
+      'Confirmar E-mail': confirmarEmail,
+      CPF: cpf,
+      'Idioma do Currículo': idioma,
+    };
+
+    setDadosCadastrados(dados);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.titulo}>Cadastro</Text>
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Gênero"
+        value={genero}
+        onChangeText={setGenero}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Data de Nascimento"
+        value={dataNascimento}
+        onChangeText={setDataNascimento}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Usuário"
+        value={usuario}
+        onChangeText={setUsuario}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Confirmar E-mail"
+        value={confirmarEmail}
+        onChangeText={setConfirmarEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="CPF"
+        value={cpf}
+        onChangeText={setCpf}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Idioma do Currículo"
+        value={idioma}
+        onChangeText={setIdioma}
+        style={styles.input}
+      />
+      <Button title="CADASTRAR" onPress={handleCadastro} />
+
+      {dadosCadastrados && (
+        <View style={styles.dadosContainer}>
+          <Text style={styles.dadosTitulo}>Dados Cadastrados:</Text>
+          {Object.keys(dadosCadastrados).map((key) => (
+            <Text key={key}>
+              {key}: {dadosCadastrados[key]}
+            </Text>
+          ))}
+        </View>
+      )}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 8,
+    marginBottom: 8,
+    width: '100%',
+  },
+  dadosContainer: {
+    marginTop: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  dadosTitulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
